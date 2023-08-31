ec9bed193da3a29d3f1ae26763f7fef176bc5cd6
NahlaElsayed <nahla6401@gmail.com>
2023-08-31 22:50:10 +0300

    first commit

--- services/orderService.js
+++ services/orderService.js
@@ -158,7 +158,7 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 const createCardOrder = async (session) => {
   const cartId = session.client_reference_id;
   const shippingAddress = session.metadata;
-  const orderPrice = session.display_items[0].amount / 100;
+  const orderPrice = session.amount_total / 100;
   const cart = await Cart.findById(cartId);
   const user = await User.findOne({ email: session.customer_email });
 
